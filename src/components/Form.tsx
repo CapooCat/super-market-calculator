e7da70856ae135ae95027ef4f2bf2fcabcb2250d
CAPOOCAT-LAPTOP\trant <tranthaitat99@gmail.com>
2024-09-07 14:36:40 +0700

    #fix bug => fix bug scroll, fix overlay

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,7 +6,7 @@ const Form = () => {
   const { fields, remove } = useFormArray();
 
   return (
-    <ul className="gap-4 px-4 divide-y-2 divide-gray-700 mb-14 divide-dashed">
+    <ul className="gap-4 px-4 mb-24 divide-y-2 divide-gray-700 divide-dashed">
       {fields.map((item: any, index: number) => (
         <Item {...item} name={`fieldArray[${index}]`} key={item.id} onRemoveClick={() => remove(index)} />
       ))}
