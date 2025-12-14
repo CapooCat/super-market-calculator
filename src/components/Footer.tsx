3a48e1034b28a0a53127d3b7699dd8911ec5ce6e
CAPOOCAT-LAPTOP\trant <tranthaitat99@gmail.com>
2025-12-14 12:03:32 +0700

    update gradient  color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ import ButtonSummary from "./ButtonSummary";
 const Footer = () => {
   return (
     <>
-      <div className="fixed inset-x-0 bottom-0 h-32 pointer-events-none -z-1 bg-gradient-to-t from-black/80 to-transparent" />
+      <div className="fixed inset-x-0 bottom-0 h-32 pointer-events-none -z-1 bg-gradient-to-t from-[#070f23]/80 to-transparent" />
       <section className="fixed bottom-0 left-0 z-20 flex justify-center w-full h-20 gap-8 text-lg font-medium">
         <ButtonReset />
         <ButtonAddItem />
